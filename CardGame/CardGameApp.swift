7fdb902ae361457a3cee3774dd5b4a47e5772661
Quoc Thinh <eeit104@gmail.com>
2021-09-03 22:32:21 +0700

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Thinh Tran on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
